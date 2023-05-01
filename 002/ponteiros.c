1a1b86b29df75487099f5a7e96c3775f6f0cf9ab
Felipe Pinheiro <santospinheiro627@gmail.com>
2023-05-01 20:04:55 -0300

    for

--- 002/ponteiros.c
+++ 002/ponteiros.c
@@ -2,15 +2,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main( void )
+int main(void)
 {
 
     int i = 0;
 
     float valor = 9.9;
 
-    char cpf[] = "162.784.458-99";
+    char cpf[] = "162.784.458-99\t";
 
     printf("%s", cpf);
 
+    for (i = 0; i < 100; i++) {
+        printf("%s", cpf);
+    }
+
+
+
 }
+
+
