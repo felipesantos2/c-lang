456f3e01933cbdc9f6fd7623b7d9ab556fae4515
Felipe Pinheiro <santospinheiro627@gmail.com>
2023-05-24 20:34:27 -0300

    printando o endereço e o valor

---
+++ 015/variaveis_e_memoria.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    int a = 10;
+    int b, c;
+
+    printf("\n\n\nOs valores das Variaveis\n");
+    printf("Os enderecos em memoria\n\n");
+    printf("&a = %p, valor = %i\n", &a, a);
+    printf("&b = %p, valor = %i\n", &b, b);
+    printf("&c = %p, valor = %i\n", &c, c);
+    printf("\n\n\n");
+
+    return 0;
+}
