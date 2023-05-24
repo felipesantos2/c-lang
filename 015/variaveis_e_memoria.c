7617c77ce7dd1c626452b57903c8e7510ceca834
Felipe Pinheiro <santospinheiro627@gmail.com>
2023-05-24 20:42:56 -0300

    big bang

--- 015/variaveis_e_memoria.c
+++ 015/variaveis_e_memoria.c
@@ -14,5 +14,16 @@ int main()
     printf("&c = %p, valor = %i\n", &c, c);
     printf("\n\n\n");
 
+    b = 20;
+    c = a + b;
+
+    printf("&a = %p, valor = %i\n", &a, a);
+    printf("&b = %p, valor = %i\n", &b, b);
+    printf("&c = %p, valor = %i\n", &c, c);
+
+    // 6487580
+    // 6487576
+    // 6487572
+    
     return 0;
 }
