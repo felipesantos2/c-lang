244d99759fdd00661e9d0cd0bc9d9b6dcce2e40b
Felipe Pinheiro <santospinheiro627@gmail.com>
2023-06-05 23:38:57 -0300

    big bang

---
+++ PARTE_03/002/alocacao.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+
+
+
+int main()
+{
+
+    int *integer = malloc(sizeof(int));
+    float *flutuante = malloc(sizeof(float));
+    
+    return 0;
+}
