ade9fa3e635a87e6a3f94e1025d9331546d0638e
Felipe Pinheiro <santospinheiro627@gmail.com>
2023-06-19 21:49:08 -0300

    Update alocacao.c

--- PARTE_03/002/alocacao.c
+++ PARTE_03/002/alocacao.c
@@ -4,13 +4,24 @@
 #include <math.h>
 #include <string.h>
 
-
-
 int main()
 {
 
-    int *integer = malloc(sizeof(int));
-    float *flutuante = malloc(sizeof(float));
-    
+    // int *integer = malloc(sizeof(int));
+    // float *flutuante = malloc(sizeof(float));
+
+    int *p = (int *) malloc(4);
+
+    if (p == NULL)
+    {
+        printf("ERROO");
+    }
+    else
+    {
+        *p = 10;
+        printf("valor-> %d endereco -> %p", *p, p);
+    }
+
+    free(p);
     return 0;
 }
