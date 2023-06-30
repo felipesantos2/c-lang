b1d1f4860c44c91939ef877c9383f075ba48f65d
Felipe Pinheiro <santospinheiro627@gmail.com>
2023-06-30 20:52:34 -0300

    big bang

---
+++ BLOCKS/001/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 3
+
+int main()
+{
+    printf("Hello World\n");
+    printf("Hello World\n");
+    printf("Hello World\n");
+    printf("Hello World\n");
+    printf("Hello World\n");
+    return 0;
+}
