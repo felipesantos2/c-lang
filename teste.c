389382fbf584a5c0942d6b61e14311518c85c995
Felipe <81455073+felipesantos2@users.noreply.github.com>
2025-04-03 21:37:06 -0300

    Create teste.c

---
+++ teste.c
@@ -0,0 +1,27 @@
+int main()
+{
+
+    // Dadoas escalares ou tipos primitivos
+    // referência: https://www.dca.fee.unicamp.br/cursos/EA876/apostila/HTML/node12.html
+
+    int n1 = 2;
+    int n2 = 1;
+
+    int sum = n1 + n2;
+
+    char c = 'f';  
+    char c1 = 'e';  
+    char c3 = 'l';  
+    char c4 = 'i';
+    char c5 = 'p';
+    char c6 = 'e';  
+
+
+    printf("sum = %d\n", sum);
+
+
+    printf("Hello, World\n");
+
+    // escalares
+    return 1;
+}
